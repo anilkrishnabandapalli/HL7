83c0e6cafb21c35a2a3370cdb8316cf101c9be3b
mitalimaniyar <mitali@jeavio.com>
2019-06-03 15:39:43 +0530

    Email PDF

--- src/main/java/com/hl7/firstPackage/HL7Controller.java
+++ src/main/java/com/hl7/firstPackage/HL7Controller.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
